ee196959abbbdfc6a84712a35e975ffccd9aa56e
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-09-28 23:44:29 +1000

    reach watcher

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <template>
   <div id="app">
     <Forms/>
+    <Computed/>
+    <List/>
+    <Templates/>
+    <Event/>
+    <Condition/>
   </div>
 </template>
 
 <script>
 import Forms from './components/Forms.vue';
+import Computed from './components/Computed.vue';
+import List from './components/List.vue';
+import Template from './components/Templates.vue';
+import Event from './components/Event.vue';
+import Condition from './components/Condition.vue';
 
 export default {
   name: 'App',
   components: {
-    Forms
+    Forms,
+    Computed,
+    List,
+    Template,
+    Event,
+    Condition
 }
 }
 </script>
---
+++ src/components/Computed.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <p>{{name.split('').reverse().join('')}}</p>
+    <p>{{reverseString}}</p>
+    <p>{{reverseStringM()}}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+    data(){
+        return{
+            name:"Hello"
+        }
+    },
+    computed:{
+            reverseString(){
+                return this.name.split('').reverse().join('')
+            }
+    },
+    //although computed and methods show same result,
+    //methods are functions, functions will be called upon new renders
+    //computed only compute again when value changed
+    methods:{
+        reverseStringM(){
+            return this.name.split('').reverse().join('')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

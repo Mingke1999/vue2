148dfd42aed2268b2b4c80909cf14a990aee851e
Mingke1999 <95258603+Mingke1999@users.noreply.github.com>
2022-10-17 23:45:32 +1100

    my directive 5 hook functions

--- src/ModuleC/MyDirectives.vue
+++ src/ModuleC/MyDirectives.vue
@@ -5,12 +5,20 @@
         <input type="text" v-focus />
         <p v-red>Hello World</p>
         <p v-green>Local Directives</p>
+        <button @click="message = 'Mounted'">Change Hooks</button>
+        <p v-myShow="flag">{{message}}</p>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+    data(){
+        return{
+            message:"Hook Functions",
+            flag: false
+        }
+    },
     directives:{
         green:{
             //hook function
--- src/ModuleC/directives/index.js
+++ src/ModuleC/directives/index.js
@@ -16,3 +16,26 @@ Vue.directive("red",{
      el.style.color = "#ff0000"
     }
 })
+
+//5 hook function
+Vue.directive("myShow",{
+    bind(el,binding,vnode,oldVnode){
+        //console.log("bound to the element")
+    },
+    inserted(el,binding,vnode,oldVnode){
+        console.log(binding.name)
+        console.log(binding.value)
+        //console.log("element inserted into its parent node")
+    },
+    update(el,binding,vnode,oldVnode){
+        console.log("before update: "+el.innerHTML)
+        //console.log("component updated")
+    },
+    componentUpdated(el,binding,vnode,oldVnode){
+        console.log("Component Updated: "+el.innerHTML)
+        //console.log("all updated")
+    },
+    unbind(el,binding,vnode,oldVnode){
+        //console.log("unbind")
+    }
+})
